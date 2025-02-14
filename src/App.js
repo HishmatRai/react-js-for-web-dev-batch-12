1e58a089897bafa70842254789f3d5baa32a5538
hishmatrai <hishmatrai.2018@gmail.com>
2025-02-14 19:49:33 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
-import React from "react";
-import './App.css'
-// function App() {
-//   return (
-//     <div>
-//       <h1>Main Page</h1>
-//     </div>
-//   );
-// }
+import React, { useState } from "react";
+import Home from "./home";
+import "./App.css";
+import About from "./About";
+//
 const App = () => {
+  // let username = "iHunar Academy";
+  let [username, setUsername] = React.useState("iHunar Academy");
+  const [email,setEmail] = useState("info@gmail.com")
+  function Login() {
+    alert("login function");
+  }
+  const signUp = () => {
+    alert("sign up function");
+  };
   return (
     <div>
-      <h1>Main Page</h1>
-      <h1 id="h1">Main Page</h1>
-      <h1>Main Page</h1>
-      <h1>Main Page</h1>
-      <h1 className="h1">Main Page</h1>
-      <h1 className="h1">Main Page</h1>
-      <h1 className="h1">Main Page</h1>
-      <h1>Main Page</h1>
-      <h1>Main Page</h1>
-      {/* <p style="color:red;"> */}
-      <p style={{ color: "red", backgroundColor: "green" }}>
-        IT services provider company managed by highly experienced information
-        technology professionals.
-      </p>
-      <form>
-        <input type="text" />
-      </form>
-      <table>
-        <tr>
-          <td>fsdf</td>
-          <td>fsdf</td>
-          <td>fsdf</td>
-          <td>fsdf</td>
-        </tr>
-      </table>
+      <h1 style={{ backgroundColor: "red", color: "white" }}>
+        Web Development Page
+      </h1>
+      <p>Username :- {username}</p>
+      <p>Email :- {email}</p>
+      {/* <button onClick="alert('update')">Update</button> */}
+      {/* <button onClick={()=>alert('update')}>Update</button> */}
+      {/* <button onClick={()=>console.log('update')}>Update</button>
+      <button onClick={()=>Login()}>Login</button>
+      <button onClick={signUp}>Sign Up</button> */}
+      {/* signUp signUp() */}
+      {/* <button onClick="username = 'abc'">Update</button> */}
+      {/* <button onClick={(username = "abc")}>Update</button> */}
+      <button onClick={()=> setUsername("Update")}>Update</button>
+      <button onClick={()=>setEmail("ihunar@gmail.com")}>Change Email</button>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <Home />
+      <About />
     </div>
   );
 };
---
+++ src/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{backgroundColor:"blue",color:"white"}}>Home Page</h1>
+    </div>
+  );
+};
+export default Home;
